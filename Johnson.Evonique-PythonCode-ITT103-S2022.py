85a24d46127dac7868429d4c252a1ad9d9ba3459
Pen3lop3 <102252781+Pen3lop3@users.noreply.github.com>
2022-05-05 00:39:27 -0500

    Add files via upload

---
+++ Johnson.Evonique-PythonCode-ITT103-S2022.py
@@ -0,0 +1,32 @@
+
+
+# Author: Evonique Johnson
+# Date Created: April 2, 2022
+# Course: ITT103
+# Purpose: This program is to calculate the commission rate of a number of workers at JamEx Limited.
+
+
+
+
+sales = input('')
+sales_class = input('')
+
+if sales_class == 1:
+    if sales <= 1000:
+        print(0.06 * sales)
+    elif 1000 < sales <= 2000:
+        print(0.07 * sales)
+    else:
+        print(0.1 * sales)
+elif sales_class == 2:
+    if sales <= 1000:
+        print(0.04 * sales)
+    else:
+        print(0.06 * sales)
+elif sales_class == 3:
+    print(0.045 * sales)
+else:
+    print("No rate is added")
+
+
+
